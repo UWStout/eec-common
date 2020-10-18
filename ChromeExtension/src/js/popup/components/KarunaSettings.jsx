5a320a7c2832cfc207114900295fe9e6f68feeaa
parsonb4929 <55289698+parsonb4929@users.noreply.github.com>
2020-10-18 13:14:44 -0500

    Added framework content to settings menus

--- ChromeExtension/src/js/popup/components/KarunaSettings.jsx
+++ ChromeExtension/src/js/popup/components/KarunaSettings.jsx
@@ -7,6 +7,15 @@ export default class KarunaSettings extends Component {
         return (
             <div>
                 <h3>Karuna Settings Content</h3>
+                <div>
+                    <h3>Collaboration timeout</h3>
+                    <input type="text" />
+                </div>
+                <div>
+                    <h3>Non project-related messages</h3>
+                    <button>thumbs down</button>
+                    <button>thumbs up</button>
+                </div>
             </div>
         );
     }
