55cc7f5031674e9506354006ff4e0c5988061743
Seth Berrier <berriers@uwstout.edu>
2020-10-24 18:54:08 -0500

    Added login support via JWTs

--- src/data.js
+++ src/data.js
@@ -1,11 +1,26 @@
+// Express router
 import Express from 'express'
+
+// JWT authorization middleware
+import { authenticateToken } from './auth.js'
 
 // Create a router to attach to an express server app
 const router = new Express.Router()
 
-// Various api routes
-router.get('/list_items', (req, res) => {
+// list_items (authorized only)
+router.get('/list_items', authenticateToken, (req, res) => {
   res.json([{ item: 1 }, { item: 2 }])
+})
+
+// list_items (authorized ADMIN only)
+router.get('/list_secret_items', authenticateToken, (req, res) => {
+  // Check if they are an admin
+  if (!req.user || req.user.type !== 'admin') {
+    return res.status(403).json({
+      error: true, message: 'not authorized (admins only)'
+    })
+  }
+  res.json([{ item: 3 }, { item: 4 }])
 })
 
 // Expose the router for use in other files
