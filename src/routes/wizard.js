8e2d058847d84bd98c58ae739d135e9f7bf7483d
Seth Berrier <berriers@uwstout.edu>
2020-11-25 12:32:01 -0600

    Debugging oz routes

--- src/routes/wizard.js
+++ src/routes/wizard.js
@@ -9,9 +9,9 @@ import Debug from 'debug'
 const debug = Debug('server:wizard')
 
 // Path for redirections
-const REDIR_PATH = '/karuna/oz/'
+let REDIRECT_PATH = '/karuna/oz/'
 if (process.argv.find((arg) => { return arg === 'DEV' })) {
-  REDIR_PATH = '/oz/'
+  REDIRECT_PATH = '/oz/'
 }
 
 // Path to raw view files
@@ -22,30 +22,35 @@ const router = new Express.Router()
 
 // List of pages that do not require authorization to access
 const unAuthPages = [
-  '/login.html', '/logout.html', '/register.html',
-  '/login.js', '/logout.js', '/register.js',
-  '/authHelper.js'
+  'login.html',
+  'logout.html',
+  'register.html',
+  'login.js',
+  'logout.js',
+  'register.js',
+  'authHelper.js'
 ]
 
 // Account login or registration (no auth required)
 router.get('/*', decodeToken, (req, res, next) => {
   // Rewrite default path
-  if (req.path === '/') { return res.redirect(REDIR_PATH + 'emeraldCity.html') }
+  if (req.path === '/') { return res.redirect(REDIRECT_PATH + 'emeraldCity.html') }
 
   // Non-authorized paths
   if (unAuthPages.indexOf(req.url) >= 0) {
     // Attempt to send file in response
-    debug(`serving insecure ${req.path}`)
+    console.log(`serving insecure ${req.path}`)
     return res.sendFile(req.path, { root: path.resolve(VIEW_PATH) })
   } else {
     // All remaining paths require authorization or redirect to logout
     if (!req.user || req.user.error) {
       // Send to logout
-      return res.redirect(REDIR_PATH + 'logout.html')
+      console.log(`Redirecting ${req.path} to logout.html`)
+      return res.redirect(REDIRECT_PATH + 'logout.html')
     }
 
     // Attempt to send file in response
-    debug(`serving secure ${req.path}`)
+    console.log(`Serving secure ${req.path}`)
     return res.sendFile(req.path, { root: path.resolve(VIEW_PATH) })
   }
 })
