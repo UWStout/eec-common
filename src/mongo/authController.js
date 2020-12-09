e1196c47ad6a6f786069d08449356b9cad91b9e1
Seth Berrier <berriers@uwstout.edu>
2020-12-08 18:10:02 -0600

    Cleaning out the old SQL code

--- src/mongo/authController.js
+++ src/mongo/authController.js
@@ -16,7 +16,7 @@ const SALT_ROUNDS = 10
  * @return {Promise} Resolves to an object with all the user data, rejects on error
  */
 export function getUserDetails (userID) {
-  return SQLHelp.getEntryFromID(userID, 'Users')
+  // TODO
 }
 
 /**
@@ -25,7 +25,7 @@ export function getUserDetails (userID) {
  * @return {Promise} Resolves to the userID or -1 if no user exists
  */
 export function emailExists (email) {
-  return SQLHelp.checkValueExistence('Users', 'email', email)
+  // TODO
 }
 
 /**
@@ -34,7 +34,7 @@ export function emailExists (email) {
  * @return {Promise} Resolves with no data if successful, rejects on error
  */
 export function removeUser (userID) {
-  return SQLHelp.deleteEntryFromID(userID, 'Users')
+  // TODO
 }
 
 /**
@@ -44,34 +44,7 @@ export function removeUser (userID) {
  * @return {Promise} Resolves to object with basic user info, rejects if invalid
  */
 export function validateUser (email, password) {
-  const userDBHandle = retrieveDBHandle('karunaData', true, true)
-  return new Promise((resolve, reject) => {
-
-    userDBHandle.get(
-      `SELECT ID, firstName, lastName, userType, passwordHash
-        FROM Users WHERE email=$email;`, { $email: email },
-      (err, row) => {
-        // Check if an error occurred
-        if (err) { return reject(err) }
-
-        // Was the user found
-        if (!row) { return reject(new Error('Invalid email or password')) }
-
-        // Validate user password
-        bcrypt.compare(password, row.passwordHash, (error, valid) => {
-          // Check if an error occurred
-          if (error) { return reject(error) }
-
-          // Was the password valid
-          if (!valid) { return reject(new Error('Invalid email or password')) }
-
-          // Return the email and row data (without password) merged
-          row.passwordHash = undefined
-          return resolve({ email: email, ...row })
-        })
-      }
-    )
-  })
+  // TODO
 }
 
 /**
@@ -84,36 +57,5 @@ export function validateUser (email, password) {
  * @return {Promise} Rejects on failure, resolves to the newly created ID on success
  */
 export function createUser (firstName, lastName, email, type, password) {
-  return new Promise((resolve, reject) => {
-    // Hash password
-    bcrypt.hash(password, SALT_ROUNDS, (err, passwordHash) => {
-      // Check if an error occurred
-      if (err) {
-        console.error('Failed to hash password')
-        console.error(err)
-        return reject(err)
-      }
-
-      // Make new user data entry
-      const userData = {
-        $firstName: firstName,
-        $lastName: lastName,
-        $email: email,
-        $type: type,
-        $pwHash: passwordHash
-      }
-
-      SQLHelp.createEntryAndReturnID('Users',
-        `INSERT INTO Users (firstName, lastName, email, userType, passwordHash)
-         VALUES ($firstName, $lastName, $email, $type, $pwHash);`,
-        userData,
-        'SELECT ID FROM Users WHERE email=$email;',
-        { $email: email }
-      ).then((userID) => {
-        return resolve(userID)
-      }).catch((err) => {
-        return reject(err)
-      })
-    })
-  })
+  // TODO
 }
