1adbf2a90e3f43c1c8882c5e9e235c4488e9fb0d
parsonb4929 <55289698+parsonb4929@users.noreply.github.com>
2021-01-04 10:09:58 -0600

    updated content of popup and removed unnecessary content

--- ChromeExtension/src/js/popup/components/popup-menus/MainMenu.jsx
+++ ChromeExtension/src/js/popup/components/popup-menus/MainMenu.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import KarunaSettings from './KarunaSettings.jsx'
 import AccountSettings from './AccountSettings.jsx'
-import EmojiHistory from './EmojiHistory.jsx'
 import Button from '@material-ui/core/Button'
 import '../CSS/Menu.css'
 
@@ -11,8 +10,7 @@ export default class MainMenu extends Component {
 
         this.state = {
             karunaSettingsClicked: false,
-            accountSettingsClicked: false,
-            emojiHistoryClicked: false
+            accountSettingsClicked: false
         }
     }
 
@@ -28,25 +26,17 @@ export default class MainMenu extends Component {
         });
     }
 
-    handleEmojiHistoryClick() {
-        this.setState({
-            emojiHistoryClicked: true
-        });
-    }
-
     handleReturnClick() {
         this.setState({
             karunaSettingsClicked: false,
-            accountSettingsClicked: false,
-            emojiHistoryClicked: false
+            accountSettingsClicked: false
         });
     }
 
     render () {
         const karunaClicked = this.state.karunaSettingsClicked;
         const accountClicked = this.state.accountSettingsClicked;
-        const emojiHistClicked = this.state.emojiHistoryClicked;
-        let karunaSettingsMenu, accountSettingsMenu, emojiHistMenu;
+        let karunaSettingsMenu, accountSettingsMenu;
 
         if (this.state.karunaSettingsClicked) {
             karunaSettingsMenu = <KarunaSettings />
@@ -54,14 +44,12 @@ export default class MainMenu extends Component {
         if(this.state.accountSettingsClicked) {
             accountSettingsMenu = <AccountSettings />
         }
-        if(this.state.emojiHistoryClicked) 
-            emojiHistMenu = <EmojiHistory />
 
         return (
             <div id="mainMenu">
                 <div>
                     <ul id="karunaHead">
-                        {karunaClicked || accountClicked || emojiHistClicked
+                        {karunaClicked || accountClicked
                             ? <li>
                                 <Button 
                                     id="returnButton" 
@@ -76,17 +64,8 @@ export default class MainMenu extends Component {
                         <li><Button id="returnButton">X</Button></li>
                     </ul>
                 </div>
-                {!karunaClicked && !accountClicked && !emojiHistClicked ?
+                {!karunaClicked && !accountClicked ?
                 <div>
-                    <div>
-                        <h3>Current Mood</h3>
-                        <Button>thumbs up</Button>
-                        <Button>thumbs down</Button>
-                    </div>
-                    <div>
-                        <h3>Emoji History</h3>
-                        <Button onClick={() => this.handleEmojiHistoryClick()}>:)O</Button>
-                    </div>
                     <div>
                         <h3>Karuna Settings</h3>
                         <Button onClick={() => this.handleKarunaClick()}>gear</Button>
@@ -96,7 +75,7 @@ export default class MainMenu extends Component {
                         <Button onClick={() => this.handleAccountClick()}>:)</Button>
                     </div>
                 </div>
-                : karunaSettingsMenu || accountSettingsMenu || emojiHistMenu
+                : karunaSettingsMenu || accountSettingsMenu
                 }
             </div>
         );
