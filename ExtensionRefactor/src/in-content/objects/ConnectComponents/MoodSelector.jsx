126b4130c53fb20b03c195198111c8b8ffaccdee
parsonb4929 <55289698+parsonb4929@users.noreply.github.com>
2021-01-28 12:24:04 -0600

    Mood selector change from menu to transparent modal; adheres to UX more closely

--- ExtensionRefactor/src/in-content/objects/ConnectComponents/MoodSelector.jsx
+++ ExtensionRefactor/src/in-content/objects/ConnectComponents/MoodSelector.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { MenuItem, Menu, Button, IconButton } from '@material-ui/core'
+import { MenuItem, Modal, IconButton } from '@material-ui/core'
 import Emoji from './Emoji.jsx'
 
 const emoji = [
@@ -11,49 +11,50 @@ const emoji = [
 // Menu of moods for user selection
 export default function MoodSelect () {
   const [mood, setMood] = useState(0)
-  const [anchorEl, setAnchorEl] = useState(null)
-
-  // Sets the anchor element on click
-  const handleButtonClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+  const [open, setOpen] = useState(false)
 
   // Sets mood and closes menu
   const handleMenuItemClick = (event, index) => {
     setMood(index)
-    setAnchorEl(null)
+    setOpen(false)
   }
 
-  // Closes menu without setting mood
-  const handleClose = () => {
-    setAnchorEl(null)
+  const handleOpen = () => {
+    setOpen(true)
   }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const body = (
+    <div className='mood-modal'>
+      {emoji.map((emote, index) => (
+        <MenuItem
+          key={index}
+          selected={index === mood}
+          onClick={(event) => handleMenuItemClick(event, index)}
+        >
+          {emote}
+        </MenuItem>
+      ))}
+    </div>
+  )
 
   return (
     <div>
       <span>
-        <IconButton size='small' aria-controls='mood-menu' aria-haspopup='true' onClick={handleButtonClick}>
+        <IconButton size='small' aria-controls='mood-menu' aria-haspopup='true' onClick={handleOpen}>
           {emoji[mood]}
         </IconButton>
         mood
       </span>
-      <Menu
-        id='mood-menu'
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
+      <Modal
+        open={open}
         onClose={handleClose}
       >
-        {emoji.map((emote, index) => (
-          <MenuItem
-            key={index}
-            selected={index === mood}
-            onClick={(event) => handleMenuItemClick(event, index)}
-          >
-            {emote}
-          </MenuItem>
-        ))}
-      </Menu>
+        {body}
+      </Modal>
     </div>
   )
 }
