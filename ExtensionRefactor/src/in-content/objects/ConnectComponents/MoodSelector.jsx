7175ced6ad21f6a0b14d81936264b653362773dd
parsonb4929 <55289698+parsonb4929@users.noreply.github.com>
2021-01-29 12:36:27 -0600

    mood selector share-with-team dialog polish

--- ExtensionRefactor/src/in-content/objects/ConnectComponents/MoodSelector.jsx
+++ ExtensionRefactor/src/in-content/objects/ConnectComponents/MoodSelector.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Modal, IconButton } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Modal, IconButton, Button, Checkbox } from '@material-ui/core'
 import Emoji from './Emoji.jsx'
 
 // placeholder for dynamic database filling
@@ -54,10 +55,22 @@ const emoji = [
   <Emoji symbol='🙁' label='sad' />,
 ]
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2),
+  },
+}));
+
 // Menu of moods for user selection
 export default function MoodSelect () {
+  const classes = useStyles()
   const [mood, setMood] = useState(0)
   const [open, setOpen] = useState(false)
+  const [shareDialog, setDialogOpen] = useState(false)
 
   // Sets mood and closes menu
   const handleMenuItemClick = (event, index) => {
@@ -75,7 +88,17 @@ export default function MoodSelect () {
     setOpen(false)
   }
 
-  // maps the modal body with the desired emotes
+  // Opens the share-with-team dialog
+  const handleShareOpen = () => {
+    setDialogOpen(true)
+  }
+
+  // Closes the share-with-team dialog
+  const handleShareClose = () => {
+    setDialogOpen(false)
+  }
+
+  // Maps the modal body with the desired emotes
   const body = (
     <div className='mood-modal'>
       {emoji.map((emote, index) => (
@@ -83,11 +106,35 @@ export default function MoodSelect () {
           size='small'
           key={index}
           selected={index === mood}
-          onClick={(event) => handleMenuItemClick(event, index)}
+          onClick={(event) => {
+            handleMenuItemClick(event, index)
+            handleShareOpen()
+          }}
         >
           {emote}
         </IconButton>
       ))}
+    </div>
+  )
+  
+  // Confirmation modal allows user to share feelings with team or keep private
+  const shareModal = (
+    <div id='mood-share-modal' className={classes.paper}>
+      <p>Do you want to share your feelings with the rest of the team?</p>
+      <span>
+      <Button onClick={handleShareClose} size='small'>
+          No, Keep Private
+        </Button>
+        <Button onClick={handleShareClose} size='small'>
+          Yes, Share
+        </Button>
+      </span>
+      <div>
+        <span className='small-text'>
+          <Checkbox />
+          Do not show this message in the future
+        </span>
+      </div>
     </div>
   )
 
@@ -105,6 +152,12 @@ export default function MoodSelect () {
         onClose={handleClose}
       >
         {body}
+      </Modal>
+      <Modal 
+        open={shareDialog}
+        onClose={handleShareClose}
+      >
+        {shareModal}
       </Modal>
     </div>
   )
