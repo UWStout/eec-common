5e5e385861cd4e1f3d01f16c439afd85202e7b23
Seth Berrier <berriers@uwstout.edu>
2021-02-13 15:18:57 -0600

    Styling and JSON tweaks

--- views/wizard/cannedMessagesSidebar.js
+++ views/wizard/cannedMessagesSidebar.js
@@ -16,13 +16,14 @@ export function makeMessageGroup (groupIndex, clickCallback, parentID, headerTex
 
 function makeCardHeader (headerText, headingID, collapseID) {
   // Build the outer container tags
-  const headerDiv = $('<div>').attr('id', headingID).addClass('card-header')
+  const headerDiv = $('<div>').attr('id', headingID).addClass('card-header p-0')
   const headerTxt = $('<h2>').addClass('mb-0')
 
   // Build the central button with text
   const headerBtn = $('<button>').attr('type', 'button').addClass('btn btn-link collapsed')
   headerBtn.attr('data-toggle', 'collapse').attr('data-target', '#' + collapseID)
   headerBtn.attr('aria-expanded', false).attr('aria-controls', collapseID)
+  headerBtn.css('margin-top', '-10px').css('width', '100%').css('text-align', 'left')
   headerBtn.text(headerText)
 
   // Assemble and return
@@ -35,7 +36,7 @@ function makeCardBody (choices, clickCB, headingID, collapseID, listID, parentID
   const bodyCollapseDiv = $('<div>').attr('id', collapseID).addClass('collapse')
   bodyCollapseDiv.attr('data-parent', '#' + parentID).attr('aria-labelledby', headingID)
 
-  const bodyCardDiv = $('<div>').addClass('card-body')
+  const bodyCardDiv = $('<div>').addClass('card-body py-1')
   const bodyListGroup = makeListGroup(choices, clickCB, listID)
 
   bodyCardDiv.append(bodyListGroup)
@@ -47,7 +48,7 @@ function makeListGroup (choices, clickCB, listID) {
   const listGroupUL = $('<ul>').attr('id', listID).addClass('list-group list-group-flush')
 
   choices.forEach((choice) => {
-    const choiceItem = $('<li>').addClass('list-group-item').text(choice.name)
+    const choiceItem = $('<li>').addClass('list-group-item px-0 py-1').text(choice.name)
     choiceItem.attr('data-message', choice.message)
     choiceItem.attr('data-overwrite', choice.overwrite)
     choiceItem.css('cursor', 'pointer')
