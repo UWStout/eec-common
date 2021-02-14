e8f765a3ed04a0e019f416bc54ea8a0f644524ff
Seth Berrier <berriers@uwstout.edu>
2021-02-14 09:54:27 -0600

    Removed legacy 'ChromeExtension' folder - Replaced by 'ExtensionRefactor' - See history in github to recover

--- ChromeExtension/iconResizer.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-pushd assets
-magick convert icon-512.png -resize 50%   icon-256.png
-magick convert icon-256.png -resize 50%   icon-128.png
-magick convert icon-128.png -resize 50%   icon-64.png
-magick convert icon-128.png -resize 37.5% icon-48.png
-magick convert icon-64.png  -resize 25%   icon-16.png
-popd
--- ChromeExtension/src/js/background.js
+++
@@ -1,9 +0,0 @@
-// Import all the setup functions needed
-import { announceSession } from './backgroundModules/SocketComms.js'
-import { setupExtensionCommunication } from './backgroundModules/ExtensionComms.js'
-import { setupWebRequests } from './backgroundModules/WebRequests.js'
-
-// Setup each module of the background script
-announceSession()
-setupExtensionCommunication()
-setupWebRequests()
