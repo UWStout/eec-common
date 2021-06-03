0f68fcb292aa28526880454e0064bd7cb035f6b5
Seth Berrier <berriers@uwstout.edu>
2021-06-03 10:39:08 -0500

    Updating test data maker to use ESM - Also exported the 'anywhere@email.com' user

--- testData/dbHelper.js
+++ testData/dbHelper.js
@@ -1,19 +1,20 @@
-const MongoClient = require('mongodb').MongoClient
+import MongoDB from 'mongodb'
+const { MongoClient } = MongoDB
 
 // URL of the database server
 const DB_SERVER_URL = 'mongodb://localhost:27017'
 
-exports.connectDB = () => {
+export function connectDB () {
   // Attempt to connect
   console.log(`Connecting to '${DB_SERVER_URL}'`)
   return MongoClient.connect(DB_SERVER_URL, { useUnifiedTopology: true })
 }
 
-exports.closeDB = (DB) => {
+export function closeDB (DB) {
   return DB.close()
 }
 
-exports.clearCollection = (DB, collectionName) => {
+export function clearCollection (DB, collectionName) {
   return new Promise((resolve, reject) => {
     // Clear the collection
     console.log(`> Deleting all documents from '${collectionName}' collection`)
@@ -26,7 +27,7 @@ exports.clearCollection = (DB, collectionName) => {
   })
 }
 
-exports.insertAllInCollection = (DB, collectionName, data) => {
+export function insertAllInCollection (DB, collectionName, data) {
   return new Promise((resolve, reject) => {
     const plural = (data.length === 1 ? 'document' : 'documents')
     console.log(`> Inserting ${data.length} ${plural} into '${collectionName}' collection`)
