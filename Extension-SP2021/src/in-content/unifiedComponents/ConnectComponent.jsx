c3adc092032248903bde4e0792e811658c69f55e
Seth Berrier <berriers@uwstout.edu>
2021-06-17 16:44:21 -0500

    Fix To MS Teams Integration

--- Extension-SP2021/src/in-content/unifiedComponents/ConnectComponent.jsx
+++ Extension-SP2021/src/in-content/unifiedComponents/ConnectComponent.jsx
@@ -1,4 +1,7 @@
+/* global EventEmitter3 */
+
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -7,11 +10,23 @@ import ConnectMainPanel from './ConnectMainPanel.jsx'
 
 const useStyles = makeStyles((theme) => ({
   // Styling of the root paper element
-  root: {
+  rootDiscord: {
     // Sizing to fill the page
     position: 'relative',
     width: '100%',
     height: '100%',
+
+    // Set to be in front of all the other elements
+    zIndex: 100
+  },
+  rootTeams: {
+    // Sizing to fill the page
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+
+    // Ensure it is at the top of page (not the bottom)
+    top: '-100vh',
 
     // Set to be in front of all the other elements
     zIndex: 100
@@ -21,14 +36,15 @@ const useStyles = makeStyles((theme) => ({
 // The sidebar Karuna Connect object
 export default function ConnectComponent (props) {
   // Deconstruct props and style class names
-  const { root } = useStyles()
+  const { rootDiscord, rootTeams } = useStyles()
+  const { context, emitter } = props
 
   // Is the mouse over this component
   const [mainPanelOpen, setMainPanelOpen] = React.useState(false)
 
   // Main render
   return (
-    <div className={root}>
+    <div className={(context === 'msTeams' ? rootTeams : rootDiscord)}>
       <ConnectStatusPanel hidden={mainPanelOpen} onHide={() => { setMainPanelOpen(true) }} />
       <ConnectMainPanel hidden={!mainPanelOpen} onHide={() => { setMainPanelOpen(false) }} />
     </div>
@@ -36,7 +52,6 @@ export default function ConnectComponent (props) {
 }
 
 ConnectComponent.propTypes = {
-}
-
-ConnectComponent.defaultProps = {
+  emitter: PropTypes.instanceOf(EventEmitter3).isRequired,
+  context: PropTypes.string.isRequired
 }
