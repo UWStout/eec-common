8fe5888e571d554364be56de38a53a5310678e9e
Larissa Ford <larissa@coreyford.com>
2021-06-18 16:10:32 -0500

    added snapshot testing

--- Extension-SP2021/src/tests/ConnectPanel.test.js
+++ Extension-SP2021/src/tests/ConnectPanel.test.js
@@ -3,8 +3,9 @@
  */
 
 import React from 'react'
-import { 
-  render, 
+
+import {
+  render,
   fireEvent,
   screen,
   cleanup
@@ -12,71 +13,83 @@ import {
 import '@testing-library/jest-dom/extend-expect'
 import { toBeVisible } from '@testing-library/jest-dom/matchers'
 
-import OpenArrow from '../in-content/unifiedComponents/OpenArrow.jsx'
 import ConnectStatusPanel from '../in-content/unifiedComponents/ConnectStatusPanel.jsx'
 import ConnectMainPanel from '../in-content/unifiedComponents/ConnectMainPanel.jsx'
 import ConnectComponent from '../in-content/unifiedComponents/ConnectComponent.jsx'
 
 expect.extend({ toBeVisible })
 
+it.todo('does this work?')
+
 describe('ConnectPanel', () => {
-  afterEach(cleanup)
+  afterEach(cleanup) // avoid memory leaks
 
   describe('ConnectStatusPanel', () => {
-    afterEach(cleanup)
-
-    it('shows up', () => {
-      const { getByTestId } = render(<ConnectStatusPanel hidden={true} />)
-      const connectStatusPanel = getByTestId("connectStatusPanel")
+    it('shows up correctly', () => {
+      const { getByTestId, asFragment } = render(<ConnectStatusPanel />)
+      const connectStatusPanel = getByTestId('connectStatusPanel')
       expect(connectStatusPanel).toBeInTheDocument()
+      expect(asFragment(<ConnectStatusPanel />)).toMatchSnapshot()
     })
-  
+
     it('shows a single arrow on load', () => {
-      const { getByTestId } = render(<ConnectStatusPanel hidden={true} />)
+      const { getByTestId } = render(<ConnectStatusPanel />)
       // the panel has a single arrow
       const singleArrow = getByTestId('keyboardArrowRight')
       expect(singleArrow).toBeInTheDocument()
     })
-  
+
     it('shows double arrow on hover of single arrow', () => {
-      const { getByTestId } = render(<ConnectStatusPanel hidden={true} />)
-      
+      const { getByTestId } = render(<ConnectStatusPanel />)
+
       // the panel has a single arrow
       const singleArrow = getByTestId('keyboardArrowRight')
       expect(singleArrow).toBeInTheDocument()
-  
+
       // on mouseOver, a double arrow is shown
-      fireEvent.mouseOver(singleArrow);
+      fireEvent.mouseOver(singleArrow)
       const doubleArrow = getByTestId('doubleArrow')
       expect(doubleArrow).toBeInTheDocument()
     })
 
     it('shows double arrow on hover of status panel', () => {
-      const { getByTestId } = render(<ConnectStatusPanel hidden={true} />)
-      const connectStatusPanel = getByTestId("connectStatusPanel")
+      const { getByTestId } = render(<ConnectStatusPanel />)
+      const connectStatusPanel = getByTestId('connectStatusPanel')
       // on mouseOver, a double arrow is shown
-      fireEvent.mouseOver(connectStatusPanel);
+      fireEvent.mouseOver(connectStatusPanel)
       const doubleArrow = getByTestId('doubleArrow')
       expect(doubleArrow).toBeInTheDocument()
     })
 
     it('expands on hover', () => {
-      const { getByTestId } = render(<ConnectStatusPanel hidden={true} />)
-      const connectStatusPanel = getByTestId("connectStatusPanel")
+      const { asFragment, getByTestId } = render(<ConnectStatusPanel />)
+      const connectStatusPanel = getByTestId('connectStatusPanel')
       // on mouseOver, panel expands
       fireEvent.mouseOver(connectStatusPanel)
-      expect(connectStatusPanel).toHaveStyle('right: -theme.spacing(8)')
+
+      // compare with snapshot
+      expect(asFragment()).toMatchSnapshot()
     })
 
-    it.todo('hides when mouse moves away')
+    it('hides when mouse moves away', () => {
+      const { asFragment, getByTestId } = render(<ConnectStatusPanel />)
+      const connectStatusPanel = getByTestId('connectStatusPanel')
+
+      // on mouseOver, panel expands
+      fireEvent.mouseOver(connectStatusPanel)
+      // on mouseLeave, panel retracts
+      fireEvent.mouseLeave(connectStatusPanel)
+
+      // compare with snapshot
+      expect(asFragment()).toMatchSnapshot()
+    })
   })
 
   describe('ConnectMainPanel', () => {
     it('shows up', () => {
       const { getByTestId } = render(<ConnectMainPanel hidden={false} />)
-      const mainPanel = getByTestId("connectMainPanel")
+      const mainPanel = getByTestId('connectMainPanel')
       expect(mainPanel).toBeInTheDocument()
-  
     })
     it('shows title', () => {
       render(<ConnectMainPanel hidden={false} />)
@@ -92,44 +105,43 @@ describe('ConnectPanel', () => {
 
     it('shows back arrow', () => {
       render(<ConnectMainPanel hidden={false} />)
-      const closeButton = screen.getByRole("button", { name: 'close panel' })
+      const closeButton = screen.getByRole('button', { name: 'close panel' })
       expect(closeButton).toBeInTheDocument()
     })
   })
 
   it('shows ConnectStatusPanel on load', () => {
     const { getByTestId } = render(<ConnectComponent />)
-    
+
     // the panel with the single arrow on middle right of the screen shows up
-    const connectStatusPanel = getByTestId("connectStatusPanel")
+    const connectStatusPanel = getByTestId('connectStatusPanel')
     expect(connectStatusPanel).toBeInTheDocument()
   })
 
   it('shows ConnectMainPanel on click', () => {
     const { getByTestId } = render(<ConnectComponent />)
-  
+
     // Connect status panel shows up
-    const connectStatusPanel = getByTestId("connectStatusPanel")
-    
+    const connectStatusPanel = getByTestId('connectStatusPanel')
+
     // clicking the status panel brings up the main panel
     fireEvent.click(connectStatusPanel)
-    const mainPanel = getByTestId("connectMainPanel")
+    const mainPanel = getByTestId('connectMainPanel')
     expect(mainPanel).toBeInTheDocument()
-
   })
 
   it('goes back to ConnectStatusPanel when ConnectMainPanel\'s back arrow is clicked', () => {
     const { getByTestId } = render(<ConnectComponent />)
-  
+
     // Connect status panel shows up
-    const statusPanel = getByTestId("connectStatusPanel")
-    
+    const statusPanel = getByTestId('connectStatusPanel')
+
     // clicking the status panel brings up the main panel
     fireEvent.click(statusPanel)
-    const mainPanel = getByTestId("connectMainPanel")
-    
+    const mainPanel = getByTestId('connectMainPanel')
+
     // clicking back arrow on the main panel leads back to status panel
-    const closeMainPanel = screen.getByRole("button", { name: 'close panel' })
+    const closeMainPanel = screen.getByRole('button', { name: 'close panel' })
     fireEvent.click(closeMainPanel)
     expect(statusPanel).toBeInTheDocument()
   })
