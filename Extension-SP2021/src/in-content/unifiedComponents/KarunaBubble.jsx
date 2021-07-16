a2a23950453daac248219cb59307bceb820a2484
Seth Berrier <berriers@uwstout.edu>
2021-07-16 12:34:26 -0500

    Removing manual ref in Bubble

--- Extension-SP2021/src/in-content/unifiedComponents/KarunaBubble.jsx
+++ Extension-SP2021/src/in-content/unifiedComponents/KarunaBubble.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 
 import { useRecoilState, useSetRecoilState } from 'recoil'
 import { ConnectVisibilityState, BubbleVisibilityState } from './data/globalState.js'
@@ -12,9 +12,6 @@ import FeedbackDialog from './KarunaBubble/FeedbackDialog/FeedbackDialog.jsx'
 
 // The karuna dialog bubble
 export default function KarunaBubble (props) {
-  // Forward ref for the PersistentIcon
-  const persistentIconRef = useRef()
-
   // Full state and setter for visibility of main connect panel (GLOBAL STATE)
   const setMainConnectPanelOpen = useSetRecoilState(ConnectVisibilityState)
 
@@ -63,7 +60,6 @@ export default function KarunaBubble (props) {
       cancelHide={cancelHideFeedbackDialog}
     >
       <PersistentBubble
-        ref={persistentIconRef}
         isContextIndicated={contextIndicator}
         hidden={!FeedbackDialogOpen}
         setOpen={openCloseFeedbackDialog}
