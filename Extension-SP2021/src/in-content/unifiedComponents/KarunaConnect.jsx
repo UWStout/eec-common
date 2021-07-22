ad2cff08294cee6f44a9b55f3c6195eadeb70f4c
Aram Wahler <wahlera9317@my.uwstout.edu>
2021-07-22 09:47:05 -0500

    Commiting some files I missed in the last commit.

--- Extension-SP2021/src/in-content/unifiedComponents/KarunaConnect.jsx
+++ Extension-SP2021/src/in-content/unifiedComponents/KarunaConnect.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { ConnectVisibilityState, BubbleVisibilityState, ValidUserState } from './data/globalState.js'
 
+import { Container } from '@material-ui/core'
+
 import { makeStyles } from '@material-ui/core/styles'
 
 import ConnectStatusDrawer from './KarunaConnect/ConnectStatusDrawer.jsx'
@@ -16,19 +18,21 @@ import ConnectMainDrawer from './KarunaConnect/ConnectMainDrawer.jsx'
 const useStyles = makeStyles((theme) => ({
   // Styling of the root paper element
   rootDiscord: {
+    // AIW By switching the injected element to Container, I don't think this styling is necessary.
     // Sizing to fill the page
-    position: 'relative',
-    width: '100%',
-    height: '100%'
+    // position: 'relative',
+    // width: '100%',
+    // height: '100%'
 
     // Set to be in front of all the other elements
     // zIndex: 100
   },
   rootTeams: {
+    // AIW By switching the injected element to Container, I don't think this styling is necessary.
     // Sizing to fill the page
-    position: 'relative',
-    width: '100%',
-    height: '100%',
+    // position: 'relative',
+    // width: '100%',
+    // height: '100%',
 
     // Ensure it is at the top of page (not the bottom)
     top: '-100vh'
@@ -64,7 +68,7 @@ export default function KarunaConnect (props) {
 
   // Main render
   return (
-    <div className={(context === 'msTeams' ? rootTeams : rootDiscord)}>
+    <Container disableGutters className={(context === 'msTeams' ? rootTeams : rootDiscord)}>
       {userLoggedIn &&
         <ConnectStatusDrawer
           hidden={!userLoggedIn || mainPanelOpen}
@@ -75,7 +79,7 @@ export default function KarunaConnect (props) {
           hidden={!mainPanelOpen}
           onHide={() => { setMainPanelOpen(false) }}
         />}
-    </div>
+    </Container>
   )
 }
 
